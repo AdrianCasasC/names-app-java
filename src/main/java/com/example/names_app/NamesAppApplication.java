a3a70169dfbbc2fcc038ab7172c01dfa5d44b681
AdrianCasasC <casasc.adrian@gmail.com>
2025-10-20 09:15:48 +0200

    (Fix): Add missing files to version control list

---
+++ src/main/java/com/example/names_app/NamesAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.names_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamesAppApplication.class, args);
+	}
+
+}
